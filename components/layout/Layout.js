763086abe47390122188d374e353091f9ec0c6fe
Torsten Krohn <torsten.krohn@webatspeed.com>
2023-07-27 17:49:01 +0200

    Add favourite icon

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,10 +1,12 @@
 import {Fragment} from "react";
 import Navigation from "./Navigation";
 import Footer from "./Footer";
+import Header from "./Header";
 
 const Layout = (props) => {
     return (
         <Fragment>
+            <Header/>
             <Navigation/>
             <main>{props.children}</main>
             <Footer/>
